995115be6d1f9b7d43a55b767c0c6e5554cf7574
Maksim Koshman <maksimkoshman090@gmail.com>
2021-07-21 15:21:55 +0300

    Добавил первую страницу теста

---
+++ dist/js/app.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  function overlay(show) {
+    if (show) {
+      var top = $(window).scrollTop();
+      var left = $(window).scrollLeft();
+      $(window).scroll(function () {
+        $(this).scrollTop(top).scrollLeft(left);
+      });
+    } else {
+      $(window).unbind("scroll");
+    }
+  }
+
+  $("#burgerBtn").click(function (evt) {
+    evt.preventDefault();
+    $("#burgerBtn").toggleClass("burger--active");
+    $("#nav").toggleClass("nav--active");
+    $("html").toggleClass("noscroll");
+
+    if ($(this).hasClass("active")) {
+      overlay(true);
+
+      document.ontouchmove = function (e) {
+        e.preventDefault();
+      };
+    } else {
+      overlay(false);
+
+      document.ontouchmove = function (e) {
+        return true;
+      };
+    }
+  });
+});
+
+// document.addEventListener("DOMContentLoaded", function () {});
