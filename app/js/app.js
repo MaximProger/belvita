c7cf69a495543fd1932f4f9722a9f59bd32c2128
Maksim Koshman <maksimkoshman090@gmail.com>
2021-07-22 13:55:08 +0300

    Добавил новые страницы

--- app/js/app.js
+++ app/js/app.js
@@ -43,4 +43,91 @@ $(document).ready(function () {
       prevArrow: $("#cabinetPrizesArrowPrev"),
       nextArrow: $("#cabinetPrizesArrowNext"),
     });
+
+  if ($(".select").length > 1) {
+    $("select").each(function () {
+      let $this = $(this).not(".select-search");
+      let parent = $(this).not(".select-search").parents(".select");
+      $this.select2({
+        minimumResultsForSearch: Infinity,
+        dropdownParent: parent,
+      });
+    });
+    $(".select-search").each(function () {
+      let $this = $(this);
+      let parent = $(this).parents(".select");
+      $this.select2({
+        dropdownParent: parent,
+      });
+    });
+  } else {
+    $("select").select2({
+      minimumResultsForSearch: Infinity,
+      dropdownParent: $(".select"),
+    });
+  }
+
+  function checkValidate() {
+    var form = $("form");
+
+    $.each(form, function () {
+      $(this).validate({
+        ignore: [],
+        errorClass: "error",
+        validClass: "success",
+        rules: {
+          name: {
+            required: true,
+          },
+          email: {
+            required: true,
+            email: true,
+          },
+          phone: {
+            required: true,
+            phone: true,
+          },
+          message: {
+            required: true,
+          },
+          password: {
+            required: true,
+            normalizer: function normalizer(value) {
+              return $.trim(value);
+            },
+          },
+        },
+        errorElement: "span",
+        errorPlacement: function (error, element) {
+          var placement = $(element).data("error");
+          if (placement) {
+            $(placement).append(error);
+          } else {
+            error.insertBefore(element);
+          }
+        },
+        messages: {
+          phone: "Некорректный номер",
+          email: "Некорректный e-mail",
+        },
+      });
+    });
+    jQuery.validator.addMethod("email", function (value, element) {
+      return (
+        this.optional(element) ||
+        /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/.test(value)
+      );
+    });
+    jQuery.validator.addMethod("phone", function (value, element) {
+      return (
+        this.optional(element) || /\+7\(\d+\)\d{3}-\d{2}-\d{2}/.test(value)
+      );
+    });
+  }
+  checkValidate();
+
+  $(".faq__header").click(function () {
+    const faqItem = $(this).parents(".faq__item");
+    faqItem.toggleClass("faq__item--active");
+  });
 });
